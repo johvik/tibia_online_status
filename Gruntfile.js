eab488fe554c1d21bdc90a0fc530d5cae0d5bf9c
johvik <john.viklund@gmail.com>
2014-08-24 12:30:07 +0200

    jshint options removed

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          console: true
-        }
-      }
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
     jsbeautifier: {
       'default': {
